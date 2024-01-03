5114bcf05d50e86a1785a9c984f6786bfb8a09fa
Deca <deca2402@gmail.com>
2024-01-03 10:19:24 -0500

    Ciclos JS

---
+++ While.js
@@ -0,0 +1,16 @@
+/* 
+let numero = 0;
+
+while (numero <= 12) {
+    console.log(numero);
+    numero = numero + 2;
+}
+*/
+
+let resultado = 1;
+let contador = 0;
+while (contador < 10) {
+    resultado = resultado * 2;
+    contador = contador + 1;
+}
+console.log(resultado);
